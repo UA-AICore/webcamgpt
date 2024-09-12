ed07306069ad2875073aaa40b5bea94f6f8c989f
ElinaZhang0721 <165329016+ElinaZhang0721@users.noreply.github.com>
2024-09-12 13:52:45 -0700

    Add files via upload

---
+++ webcamGPT/webcamgpt/core.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+
+import numpy as np
+import requests
+
+from webcamgpt.utils import compose_payload
+
+load_dotenv(dotenv_path='C:/Users/yufei/Programming/AICORE/WebCamGPT/webcamGPT/.env')
+API_KEY = os.getenv('OPENAI_API_KEY')
+
+
+class OpanAIConnector:
+
+    def __init__(self, api_key: str = API_KEY):
+        if api_key is None:
+            raise ValueError("API_KEY is not set")
+        self.api_key = api_key
+
+    def simple_prompt(self, image: np.ndarray, prompt: str) -> str:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+        payload = compose_payload(image=image, prompt=prompt)
+        response = requests.post("https://api.openai.com/v1/chat/completions",
+                                 headers=headers, json=payload).json()
+
+        return response['choices'][0]['message']['content']
